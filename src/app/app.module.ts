c12421b6a640495dcd9dd0643dfad34ac1e8b323
Alice Guan <aliguan726@gmail.com>
2017-06-24 11:46:07 -0400

    Second batch of Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { QuoteListComponent } from './quote-list/quote-list.component';
 import { MyHomeComponentComponent } from './my-home-component/my-home-component.component';
 import { MyAboutComponentComponent } from './my-about-component/my-about-component.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { CountersComponent } from './counters/counters.component';
+import { ChuckNorrisPageComponent } from './chuck-norris-page/chuck-norris-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteListComponent,
     MyHomeComponentComponent,
-    MyAboutComponentComponent
+    MyAboutComponentComponent,
+    ContactListComponent,
+    ContactComponent,
+    CountersComponent,
+    ChuckNorrisPageComponent
   ],
   imports: [
     BrowserModule
