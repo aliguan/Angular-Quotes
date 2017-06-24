635f5a1c8f294481962b8537514f190133911118
Alice Guan <aliguan726@gmail.com>
2017-06-24 11:44:57 -0400

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuoteListComponent } from './quote-list/quote-list.component';
+import { MyHomeComponentComponent } from './my-home-component/my-home-component.component';
+import { MyAboutComponentComponent } from './my-about-component/my-about-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteListComponent,
+    MyHomeComponentComponent,
+    MyAboutComponentComponent
   ],
   imports: [
     BrowserModule
